568459960d80872609ef75d5dc558652cc87b73a
Max <max.hanson03@outlook.com>
2025-03-07 17:20:22 +0000

    added pathway to login page and username, still in progress

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,35 +2,54 @@ import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
 import { useAuth } from "./contexts/username";
 
-const validUsersFromApiCall = ["slickjimmy1992", "XXcharliethebeastXX", "rawr"];
+interface Users {
+  [key: string]: string;
+}
+const validUsersFromApiCall: Users = {
+  slickjimmy1992: "TheSearchers1956",
+  XXcharliethebeastXX: "Unforgiven1992",
+  rawr: "Stagecoach1939",
+};
 
 export default function Login() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isInvalidUsername, setIsInvalidUsername] = useState(false);
   const { user, setUser } = useAuth();
 
   function validUsernameCheck() {
-    if (validUsersFromApiCall.includes(username)) {
+    if (
+      validUsersFromApiCall[username] &&
+      validUsersFromApiCall[username] === password
+    ) {
       setUser(username);
+      Alert.alert("Howdy Partner!");
     } else {
       setIsInvalidUsername(true);
     }
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login {user}</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <Button title="Login" onPress={validUsernameCheck} />
-      {isInvalidUsername ? (
-        <Text>Not a valid username! Please try another:)</Text>
-      ) : null}
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login {user}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+
+        <TextInput
+          style={styles.pwInput}
+          placeholder="Enter Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Button title="Login" onPress={validUsernameCheck} />
+        {isInvalidUsername ? <Text>Not a valid user! </Text> : null}
+      </View>
+    </>
   );
 }
 
@@ -46,6 +65,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
+    width: "80%",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+
+  pwInput: {
     width: "80%",
     padding: 10,
     borderWidth: 1,
